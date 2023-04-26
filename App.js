80d0ed0239107674d6e8172c386d0eb2e15e1a23
Andrei Pereira <48697839+andreiPereira@users.noreply.github.com>
2023-04-26 16:08:24 -0300

    05 Imagem e estilos

--- App.js
+++ App.js
@@ -1,21 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StatusBar } from 'react-native';
 import Cesta from './src/telas/Cesta';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <SafeAreaView>
+      <StatusBar />
       <Cesta />
-      <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#f2f2f2',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
