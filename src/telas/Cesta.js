bce97daa601b1288f9e5c9cf5e6e3ad41d96f4c9
Andrei Pereira <48697839+andreiPereira@users.noreply.github.com>
2023-04-26 16:53:56 -0300

    Detalhes da cesta

--- src/telas/Cesta.js
+++ src/telas/Cesta.js
@@ -1,6 +1,7 @@
 import React from "react";
-import { Image, StyleSheet, Dimensions, Text } from "react-native";
+import { Image, StyleSheet, Dimensions, Text, View } from "react-native";
 import topo from '../../assets/topo.png'
+import logo from '../../assets/logo.png'
 
 const width = Dimensions.get('screen').width;
 
@@ -8,14 +9,29 @@ export default function Cesta() {
     return <>
         <Image source={topo} style={estilos.topo} />
         <Text style={estilos.titulo}> Detalhe da cesta </Text>
+        <View style={estilos.cesta}>
+            <Text style={estilos.nome}>Cesta de Verduras</Text>
+            <View style={estilos.fazenda}>
+                <Image source={logo} style={estilos.imagemFazenda} />
+                <Text style={estilos.nomeFazenda}>Jenny Jack Farm</Text>
+            </View>
+            <Text style={estilos.descricao}>
+                Uma cesta com produtos selecionados
+                cuidadosamente da fazendo direto para
+                a sua cozinha
+            </Text>
+            <Text style={estilos.preco}>R$ 40,00</Text>
+        </View>
     </>
 }
 
 const estilos = StyleSheet.create({
+
     topo: {
         width: '100%',
         height: 578 / 768 * width
     },
+
     titulo: {
         width: '100%',
         position: 'absolute',
@@ -26,5 +42,48 @@ const estilos = StyleSheet.create({
         fontWeight: 'bold',
         padding: 16,
         marginTop: '10%',
-    }
+    },
+
+    cesta: {
+        paddingVertical: 8,
+        paddingHorizontal: 16
+    },
+
+    nome: {
+        fontSize: 26,
+        lineHeight: 42,
+        color: "#464646",
+        fontWeight: 'bold',
+    },
+
+    descricao: {
+        color: '#A3A3A3',
+        fontSize: 16,
+        lineHeight: 26,
+    },
+
+    preco: {
+        color: '#2A9F85',
+        fontWeight: 'bold',
+        fontSize: 26,
+        lineHeight: 42,
+        marginTop: 8
+    },
+
+    fazenda: {
+        flexDirection: 'row',
+        paddingVertical: 12
+    },
+
+    nomeFazenda: {
+        fontSize: 16,
+        lineHeight: 26,
+        marginLeft: 12
+    },
+
+
+    imagemFazenda: {
+        width: 32,
+        height: 32
+    },
 });
